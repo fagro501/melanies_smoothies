be01deb217e43813be54b09a7599d443c69de07f
fagro501 <fagronert@gmail.com>
2024-07-07 08:20:45 +0200

    Update smoothieForm.py

--- smoothieForm.py
+++ smoothieForm.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response)
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -17,12 +16,16 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, 
                                  max_selections = 5)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
 name_on_order = st.text_input("Name on the Smoothie")
 if ingredients_list:
     ingredients_string = ""
     for fruit in ingredients_list:
         ingredients_string+=fruit + " "
+        st.subheader(fruit + " Nutrition Information")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
