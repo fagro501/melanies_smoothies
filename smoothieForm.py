ff10076f412498ea3bc73a6481862bec9d195516
fagro501 <fagronert@gmail.com>
2024-07-07 08:39:00 +0200

    Update smoothieForm.py

--- smoothieForm.py
+++ smoothieForm.py
@@ -27,8 +27,8 @@ if ingredients_list:
     for fruit in ingredients_list:
         ingredients_string+=fruit + " "
         st.subheader(fruit + " Nutrition Information")
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     time_to_insert = st.button("Submit Order")
